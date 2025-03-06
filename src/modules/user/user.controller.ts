6ac5190d18b915740fa1cf532c86c8f398996228
Min Htet Naing <decency.9991@gmail.com>
2025-03-06 23:27:20 +0630

    feat(mail): add mail service and sent contact confirmation mail

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,17 @@
 import { BadRequestException, Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ExceptionConstants } from '@app/core/exceptions/constants';
-import { UserService } from './user.service';
-import { UserDto } from './dto/user.dto';
 import EmailService from '@app/shared/mail/mail.service';
 import { contactUsTemplate } from '@app/shared/mail/template/contact-us.template';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
 
 @ApiTags('User')
 @Controller({ version: '1' })
 export class UserController {
-  constructor(private readonly userService: UserService,
-    private readonly mailService: EmailService
+  constructor(
+    private readonly userService: UserService,
+    private readonly mailService: EmailService,
   ) {}
 
   @Post()
@@ -19,12 +20,12 @@ export class UserController {
   async create(@Body() dto: UserDto) {
     try {
       const newUser = await this.userService.create(dto);
-        await this.mailService.sendMail({
-            from: 'spendwise@gmail.com',
-            to: dto.email,
-            subject: 'Contact Confirmation',
-            html: contactUsTemplate(newUser.email),
-        })
+      await this.mailService.sendMail({
+        from: 'spendwise@gmail.com',
+        to: dto.email,
+        subject: 'Contact Confirmation',
+        html: contactUsTemplate(newUser.name),
+      });
       return {
         _data: newUser,
         _metadata: {
--- src/shared/mail/template/contact-us.template.ts
+++ src/shared/mail/template/contact-us.template.ts
@@ -1,5 +1,5 @@
 export const contactUsTemplate = (mail: string) => {
-    return `
+  return `
        <!DOCTYPE html>
   <html lang="en">
   <head>
